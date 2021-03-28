72dbfef8fdbe186165a573f431de6cd2e7516a10
shift <koicom0702@gmail.com>
2021-03-28 16:47:25 +0900

    macro use

--- general/src/lib.rs
+++ general/src/lib.rs
@@ -1,7 +1,22 @@
 pub use self::general::*;
 
 mod general {
-
+    #[macro_export]
+    macro_rules! max_assign {
+        ($left:expr,$right:expr) => {
+            if $left < $right {
+                $left = $right;
+            }
+        };
+    }
+    #[macro_export]
+    macro_rules! min_assign {
+        ($left:expr,$right:expr) => {
+            if $left > $right {
+                $left = $right;
+            }
+        };
+    }
     pub fn ceil_pow2(n: u32) -> u32 {
         32 - n.saturating_sub(1).leading_zeros()
     }
@@ -10,28 +25,11 @@ mod general {
     }
 }
 
-#[macro_export]
-macro_rules! max_assign {
-    ($left:expr,$right:expr) => {
-        if $left < $right {
-            $left = $right;
-        }
-    };
-}
-#[macro_export]
-macro_rules! min_assign {
-    ($left:expr,$right:expr) => {
-        if $left > $right {
-            $left = $right;
-        }
-    };
-}
 #[test]
 fn test() {
     for i in 1..=10 {
         println!("{} {:?}", ceil_pow2(i), i.next_power_of_two())
     }
-    let mut x = 1;
-    max_assign!(x, 2);
-    dbg!(x);
+    let mut z = 1;
+    max_assign!(z,2);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub extern crate __shift_bitset as bitset;
+#[macro_use]
 pub extern crate __shift_general as general;
 pub extern crate __shift_iter_ext as iter_ext;
 pub extern crate __shift_math as math;
