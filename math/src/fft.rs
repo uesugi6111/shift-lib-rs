19038deca0f94e1fc5977b43c95c6c50c5ae5d4e
Shift0117 <koicom0702@gmail.com>
2021-05-05 00:35:16 +0900

    del

--- math/src/fft.rs
+++ math/src/fft.rs
@@ -1,22 +1,17 @@
-use crate::fp::{Fp,F998244353 as F};
+use crate::fp::{Fp, F998244353 as F};
 
 type Poly = Vec<F>;
-fn rem(f : &Poly,c:F) -> Poly {
+fn rem(f: &Poly, c: u64) -> Poly {
     todo!()
 }
-fn fft(f : &Poly,i:u64,flag : bool) -> Poly
+fn fft(f: &Poly, i: u64, flag: bool) -> Poly
 // x^n - c を状態に持ってる
 {
     let deg = f.len();
     if deg == 0 {
         return f.clone();
     }
-    let l = rem(f,i/2);
-    let r = rem(f,i/2 ); 
-<<<<<<< HEAD
+    let l = rem(f, i / 2);
+    let r = rem(f, i / 2);
     todo!()
 }
-=======
-
-}
->>>>>>> parent of 80d0762... t
--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,3 @@ pub extern crate __shift_iter_ext as iter_ext;
 pub extern crate __shift_math as math;
 pub extern crate __shift_graph as graph;
 pub extern crate __shift_data_structure as data_structure;
-pub extern crate __shift_doubling as doubling;
