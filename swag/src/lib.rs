22f9fb26bec3e3a8b0cd6f06ac21d14e3a95d055
Shift0117 <koicom0702@gmail.com>
2021-05-27 10:24:01 +0900

    aa

--- swag/src/lib.rs
+++ swag/src/lib.rs
@@ -10,7 +10,7 @@ mod swag {
         right_offset: usize,
     }
     impl<'a, T: SemiGroup> SWAG<'a, T> {
-        pub fn new(a: &'a [<T as SemiGroup>::S]) -> Self {
+        pub fn new(a: &'a [T::S]) -> Self {
             SWAG {
                 data: a.iter(),
                 front_stack: Vec::new(),
